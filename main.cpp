86ff4c1c7a8bb56b6b520a89b3d59c6279f9f314
Suhail Ali <76821581+SuhailAli1999@users.noreply.github.com>
2023-09-27 23:46:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,6 +31,14 @@ ll fix_mod(ll a, ll b){
     return (a % b + b) % b;
 }
 
+bool is_perfect_square(long double x){
+    if (x >= 0) {
+        long long sr = sqrt(x);
+        return (sr * sr == x);
+    }
+    return false;
+}
+
 vector<int> get_divisors(int n){ // o(sqrt(n)) //factoring
     vector<int> ans;
     for(int i=1; i*i<= n; i++){
@@ -66,7 +74,7 @@ void sieve(int isPrime[], int N){ // o(n) -- o(n log(log(n)))
     fill(isPrime, isPrime+N, true);
     isPrime[0] = isPrime[1] = false;
     for(int p=2; p*p<N; p++){
-        if (isPrime[p]){
+        if (isPrime[p] == 1){
             for(int m = p*p; m<N; m+=p){
                 isPrime[m] = false;
             }
@@ -74,13 +82,12 @@ void sieve(int isPrime[], int N){ // o(n) -- o(n log(log(n)))
     }
 }
 
-void prime_factorization_sieve(int isPrime[], int N){ // o(n) -- o(n log(log(n)))
-    fill(isPrime, isPrime+N, true);
-    isPrime[0] = isPrime[1] = false;
+void prime_factorization_sieve(vector<int> isPrime[], int N){ // o(n) -- o(n log(log(n)))
+
     for(int p=2; p<N; p++){
-        if (isPrime[p] == 1){
-            for(int m = p; m<N; m+=p){
-                if(isPrime[m] == 1) isPrime[m]=p;
+        if (isPrime[p].empty()){
+            for(int m=p; m<N; m+=p){
+                isPrime[m].push_back(p);
             }
         }
     }
@@ -101,13 +108,23 @@ vector<int> prime_factorization(ll n){ //o(sqrt(n))
     return ans;
 }
 
-int fastPower(int base, int power){
+ll factorial(ll n){ // n! ---> o(n)
+    vector<ll> v(n+1);
+    v[0] = 1;
+    for(int i=1; i<=n; i++){
+        v[i] = v[i-1] * i;
+    }
+    return v[n];
+}
+
+///////////////////////////////////////
+ll fastPower(ll base, ll power){
     if(power == 0) return 1;
     if(power == 1) return base;
-    ll half = fastPower(base, power/2);
-    ll ans = half*half;
+    ll half = fastPower(base, power/2) % mod;
+    ll ans = (half*half) % mod;
     // if base odd
-    if(power%2) ans*=base;
+    if(power%2) ans = (ans * base) % mod;
     return ans;
 }
 
@@ -117,24 +134,25 @@ ll modInv(ll n) {
 
 ll fact[N], inv[N];
 
-ll factorial(ll n){ // n! ---> o(n)
-    vector<ll> v(n+1);
-    v[0] = 1;
-    for(int i=1; i<=n; i++){
-        v[i] = v[i-1] * i;
-        v[i]%= mod;
+void precomputeFactorials(){
+    fact[0] = 1;
+    inv[0] = 1;
+
+    for (int i = 1; i <= N; i++) {
+        fact[i] = (fact[i - 1] * i) % mod;
         inv[i] = fastPower(fact[i], mod - 2);
     }
-    return v[n];
 }
 
 ll nCr(ll n, ll r) {
-    return ((fact[n] * inv[r]) % mod * inv[n - r]) % mod;
+    return ( (fact[n] * inv[r]) % mod * inv[n - r]) % mod;
 }
 
 ll nPr(ll n, ll r) {
     return ((fact[n] * inv[n - r])) % mod;
 }
+
+///////////////////////////////////////////
 
 //complete search
 void complete_search (int n){
@@ -150,13 +168,26 @@ void complete_search (int n){
         cout << endl;
     }
 }
+//all permission using backtracking
+void bt(int i, vector<int> v, int vis[], int n){
+    if(v.size() == n){
 
-bool is_perfect_square(long double x){
-    if (x >= 0) {
-        long long sr = sqrt(x);
-        return (sr * sr == x);
+        for(auto it : v){
+            cout << it << " ";
+        }
+        cout << endl;
     }
-    return false;
+
+   for(int j=1; j<=n; j++){
+        if(!vis[j]){
+            vis[j] = 1;
+            v.push_back(j);
+            bt(i+1);
+            vis[j] = 0;
+            v.pop_back();
+        }
+   }
+
 }
 
 void solve(){
